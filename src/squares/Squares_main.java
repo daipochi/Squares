95ac938b68f59cd065142cb59630d0e1c255ff3e
daichi <daichi@DESKTOP-5QJ1H1K>
2018-01-29 17:19:31 +0900

    writeCSV

--- src/squares/Squares_main.java
+++ src/squares/Squares_main.java
@@ -1,9 +1,12 @@
 package squares;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Arrays;
 
 
@@ -19,6 +22,7 @@ public class Squares_main {
 		double y[] = smain.getCSV1("csv/y.csv",7);
 		double theta[] = new double[2];
 		double value[] = new double[100];
+		double value2[][] = new double[100][4];
 		Squares_lib slib = new Squares_lib(x, y);
 		for(int i = 0; i < 100; i++){
             theta = slib.getDx();
@@ -26,10 +30,16 @@ public class Squares_main {
             System.out.println(i+" : theta[1] = "+theta[1]);
             System.out.println(i+" : Objective function = "+slib.getObject());
             value[i]=slib.getObject();
+            value2[i][0] = i;
+            value2[i][1] = slib.getObject();
+            value2[i][2] = theta[0];
+            value2[i][3] = theta[1];
         }
 		Graph graph = new Graph(value);
 		graph.setBounds(5,5,655,455);
 		graph.setVisible(true);
+		
+		smain.writeCSV("csv/result.csv",value2);
 	}
 	
     public double[] getCSV1(String path, int n) { //CSVから1次元データ取り込み(n:データ数)
@@ -56,7 +66,39 @@ public class Squares_main {
 
     }
 
-
+    	public void writeCSV(String path,double[][] data) {
+    		try {
+				FileWriter fw = new FileWriter(path, true); //true:追記、false:上書き
+				PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
+				//内容を指定する
+				pw.print("id");
+				pw.print(",");
+				pw.print("Objective");
+				pw.print(",");
+				pw.print("theta0");
+				pw.print(",");
+				pw.print("theta1");
+				
+				pw.println();
+				for (int i = 0; i < data.length; i++) {
+					for(int j=0;j<data[0].length;j++) {
+						pw.print(data[i][j]);
+						pw.print(",");
+					}
+					
+					//pw.print(",");
+					pw.println();
+				}
+				pw.println();
+				//ファイルに書き出す
+				pw.close();
+				
+				System.out.println("出力完了 : " + path);
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+    	}
 
 
 }
